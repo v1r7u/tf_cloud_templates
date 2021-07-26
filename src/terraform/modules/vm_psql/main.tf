460c67f8bfdbc2c8209acda8702afe4d113d84b6
Kliushnikov Igor <igor.kliushnikov@hotmail.com>
2021-07-26 20:46:30 +0200

    remove not needed files

--- src/terraform/modules/vm_psql/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-rg"
-  location = var.location
-}
-
-resource "tls_private_key" "vm_admin" {
-  algorithm = "RSA"
-  rsa_bits = 4096
-}
